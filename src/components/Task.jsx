d2e1924bc7dd4e15f1448e1e06baae98b2859b69
Faizan <faizanalam980@gmail.com>
2024-04-20 14:07:35 +0530

    added diff. colors for diff. priority

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -21,7 +21,7 @@ function Task({ task, setEditingTask, setIsModalOpen }) {
     <div className="flex justify-between items-center bg-white p-4 my-2 rounded shadow">
       <div>
         <h2 className="text-lg font-bold">{task.title}</h2>
-        <p className={`text-sm text-center px-2 text-white rounded-xl ${priorityColor}`}>{task.priority}</p>
+        <p className={`text-sm text-center px-2 text-white rounded-xl w-20 ${priorityColor}`}>{task.priority}</p>
       </div>
       <div>
         <button onClick={handleEdit} className="text-pink-600 mr-2">
