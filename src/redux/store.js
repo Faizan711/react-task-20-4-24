04a968d75c5cd41922be9b8813b04fd8594e7fb9
Faizan <faizanalam980@gmail.com>
2024-04-20 12:45:05 +0530

    created store for using redux

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+// src/redux/store.js
+import { configureStore } from '@reduxjs/toolkit'
+import tasksReducer from './tasksSlice'
+
+export default configureStore({
+  reducer: {
+    tasks: tasksReducer
+  }
+})
